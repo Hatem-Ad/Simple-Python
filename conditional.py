d4db7289cd79410a633849463db320b8d345ef38
Hatem-Ad <45299723+Hatem-Ad@users.noreply.github.com>
2020-04-01 22:53:39 +0100

    Create conditional.py

---
+++ conditional.py
@@ -0,0 +1,10 @@
+x = 3
+y = 3
+z = 4
+if x==y :
+    if x != z and y < z :
+        if z > x and z < y :
+            if x+y > z :
+                print("this is the point !")
+else:
+    print("this is the exit way !")
